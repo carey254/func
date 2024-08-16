d0d03d65204492805d4bc2d957a778b9575591ec
carey254 <115879669+carey254@users.noreply.github.com>
2024-08-16 16:29:13 +0300

    Add files via upload

---
+++ userr_input.py
@@ -0,0 +1,9 @@
+def user_input():
+    name = input("What's your name? ")
+    age = input("How old are you? ")
+    location = input("Where are you from? ")
+
+    print(f"Hello {name}, you are {age} years old and you live in {location}.")
+    print("It was a pleasure to know you. Thank you!")
+
+user_input()
